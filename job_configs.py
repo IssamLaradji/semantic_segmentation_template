f7286eaafb5d5bc81e2f7d6bb87f6e24db026a08
Issam H. Laradji <IssamLaradji@users.noreply.github.com>
2021-10-14 17:45:42 -0400

    Update job_configs.py

--- job_configs.py
+++ job_configs.py
@@ -1,11 +1,6 @@
 import os
 
-if os.environ['EAI_ACCOUNT_ID'] == "7c445bd5-8d23-48dd-963e-b0480d238c8a":
-    user = "pau"
-elif os.environ['EAI_ACCOUNT_ID'] == "68fdc833-1eaa-4b60-9b64-ac27814e4b61": 
-    user = 'sai'
-else:
-    user = 'issam'
+user = 'issam'
 
 JOB_CONFIG =  {'account_id':os.environ['EAI_ACCOUNT_ID'] ,
             'image': 'registry.console.elementai.com/snow.colab/ssh',
